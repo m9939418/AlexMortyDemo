4d9a1415460fde3eb74dadde2fb71de9f1216510
AlexYang <alexyang@gamania.com>
2023-03-20 10:51:06 +0800

    Initial Commit

---
+++ AlexMortyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlexMortyDemo
+//
+//  Created by AlexYang on 2023/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
